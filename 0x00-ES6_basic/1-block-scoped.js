ce0f349292ec9380176b0ef57cb2d9ba9cadf6cd
Achrafsadeq <achrafsadeqq@gmail.com>
2025-04-14 16:17:49 +0000

    Controls variable scoping based on a condition.

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+/* Using let for block-scoping to prevent variable overwriting */
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+
+  return [task, task2];
+}
