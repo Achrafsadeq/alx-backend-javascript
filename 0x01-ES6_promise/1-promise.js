c51dd3cfdb08fc5026c83bae1529723255dadc1c
Achrafsadeq <achrafsadeqq@gmail.com>
2025-04-16 15:00:43 +0000

    Returns a promise that resolves or rejects based on the success parameter.

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,9 +3,8 @@ export default function getFullResponseFromAPI(success) {
     /* Immediately reject if success is false */
     if (!success) {
       reject(new Error('The fake API is not working currently'));
-      return; // Important: return to prevent further execution
+      return;
     }
-    
     /* Resolve with success response if success is true */
     resolve({
       status: 200,
