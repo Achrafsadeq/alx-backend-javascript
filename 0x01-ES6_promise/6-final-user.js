ee55373fc1afaeb6d7f36d6e7d47d99653aebbed
Achrafsadeq <achrafsadeqq@gmail.com>
2025-04-16 16:41:19 +0000

    implementation of the handleProfileSignup function that handles multiple promises and returns their settlement results.

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+/* Import the signUpUser function from 4-user-promise module */
+import signUpUser from './4-user-promise';
+/* Import the uploadPhoto function from 5-photo-reject module */
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
+}
