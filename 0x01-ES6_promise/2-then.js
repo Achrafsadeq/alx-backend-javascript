7ab72ce26263e38bf39c4baa5b6e5e8e559e1b87
Achrafsadeq <achrafsadeqq@gmail.com>
2025-04-16 15:29:15 +0000

    Handle resolved and rejected API responses with logging.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      /* On success, return a response object */
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      /* On failure, return an empty Error object */
-      return Error();
-    })
-    .finally(() => {
-      /* Always log this message regardless of success or failure */
-      console.log('Got a response from the API');
-    });
+	/* On success, return a response object */
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+	/* Always log this message regardless of success or failure */
+    .finally(() => { console.log('Got a response from the API'); });
 }
