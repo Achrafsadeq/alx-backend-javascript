5346a713b4b11dfa90f5e756a1763d604340a491
Achrafsadeq <achrafsadeqq@gmail.com>
2025-04-16 15:30:32 +0000

    update

--- 0x01-ES6_promise/2-then.js
+++
@@ -1,8 +0,0 @@
-export default function handleResponseFromAPI(promise) {
-  return promise
-	/* On success, return a response object */
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => Error())
-	/* Always log this message regardless of success or failure */
-    .finally(() => { console.log('Got a response from the API'); });
-}
