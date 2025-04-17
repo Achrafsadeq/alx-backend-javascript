106de9c0ff75476ba508a6c2b5e69193b0d80e41
Achrafsadeq <achrafsadeqq@gmail.com>
2025-04-17 13:12:10 +0000

    create initializeRooms function that returns 3 ClassRoom objects.

---
+++ 0x02-ES6_classes/1-main.js
@@ -0,0 +1,10 @@
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
+/* Output:
+[
+  ClassRoom { _maxStudentsSize: 19 },
+  ClassRoom { _maxStudentsSize: 20 },
+  ClassRoom { _maxStudentsSize: 34 }
+]
+*/
