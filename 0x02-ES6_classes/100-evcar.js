0d3f72652b5c363ff32d4c4bb9c170c16e9994a6
Achrafsadeq <achrafsadeqq@gmail.com>
2025-04-17 21:51:12 +0000

    Implement EVCar extending Car (advanced)

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,15 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
+// Define and export the EVCar class, which extends Car
 export default class EVCar extends Car {
+  // Constructor initializes brand, motor, color, and range for the EV car
   constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
+    super(brand, motor, color); // Call the parent class constructor with brand, motor, and color
+    this._range = range; // Initialize the range of the electric car
   }
 
+  // Override cloneCar method to create a new instance of the Car class (Note: This is not cloning EVCar)
   cloneCar() {
-    // Return a base Car instance (not EVCar)
-    return new Car(this._brand, this._motor, this._color);
+    return new Car(); // Creates a new Car instance (not EVCar instance)
   }
 }
