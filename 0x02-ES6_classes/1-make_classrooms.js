ab7500581f7c1d1bd6b414a1605881036a49a5fb
Achrafsadeq <achrafsadeqq@gmail.com>
2025-04-17 15:45:26 +0000

    create initializeRooms function that returns 3 ClassRoom objects.

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+/* initializeRooms function that returns 3 ClassRoom objects */
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const arr = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return (arr);
+}
