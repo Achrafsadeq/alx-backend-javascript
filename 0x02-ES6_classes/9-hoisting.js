2db7a06fd8199b2ba413a6487488c080eef39864
Achrafsadeq <achrafsadeqq@gmail.com>
2025-04-18 11:10:33 +0000

    d

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,4 @@
+// Define the ALXClass
 export class ALXClass {
   constructor(year, location) {
     this._year = year;
@@ -13,6 +14,7 @@ export class ALXClass {
   }
 }
 
+// Define the StudentALX class
 export class StudentALX {
   constructor(firstName, lastName, alxClass) {
     this._firstName = firstName;
@@ -33,13 +35,19 @@ export class StudentALX {
   }
 }
 
+// Create class instances first
 const class2019 = new ALXClass(2019, 'San Francisco');
 const class2020 = new ALXClass(2020, 'San Francisco');
 
+// Create student instances
 const student1 = new StudentALX('Guillaume', 'Salva', class2020);
 const student2 = new StudentALX('John', 'Doe', class2020);
 const student3 = new StudentALX('Albert', 'Clinton', class2019);
 const student4 = new StudentALX('Donald', 'Bush', class2019);
 const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
-export default [student1, student2, student3, student4, student5];
+// Create and export the listOfStudents array
+export const listOfStudents = [student1, student2, student3, student4, student5];
+
+// Default export should be the same array
+export default listOfStudents;
