d501efdca8f67bea71c052db8702006827a5ee81
Achrafsadeq <achrafsadeqq@gmail.com>
2025-06-10 13:32:11 -0700

    More complex HTTP server with routing and CSV reading

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -11,18 +11,18 @@ function countStudents(path) {
   return fs.readFile(path, 'utf8')
     .then(data => {
       const lines = data.split('\n').filter(line => line.trim() !== '');
-      
+
       if (lines.length <= 1) {
         return 'Number of students: 0';
       }
-      
+
       const students = lines.slice(1).map(line => {
         const [firstname, lastname, age, field] = line.split(',');
         return { firstname, lastname, age, field };
       });
-      
+
       let result = `Number of students: ${students.length}\n`;
-      
+
       const fieldGroups = {};
       students.forEach(student => {
         if (!fieldGroups[student.field]) {
@@ -30,12 +30,12 @@ function countStudents(path) {
         }
         fieldGroups[student.field].push(student.firstname);
       });
-      
+
       Object.keys(fieldGroups).forEach(field => {
         const names = fieldGroups[field];
         result += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
       });
-      
+
       return result.trim();
     })
     .catch(() => {
@@ -46,20 +46,20 @@ function countStudents(path) {
 const app = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const pathname = parsedUrl.pathname;
-  
+
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  
+
   if (pathname === '/') {
     res.end('Hello Holberton School!');
   } else if (pathname === '/students') {
     const databasePath = process.argv[2];
     res.write('This is the list of our students\n');
-    
+
     if (!databasePath) {
       res.end('Cannot load the database');
       return;
     }
-    
+
     countStudents(databasePath)
       .then(data => {
         res.end(data);
