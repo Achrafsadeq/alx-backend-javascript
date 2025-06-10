167afc5c220632e7f0066a7e1fab51c0e09d1825
Achrafsadeq <achrafsadeqq@gmail.com>
2025-06-10 08:57:27 -0700

    A program that uses process.stdin to read user input.

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+/**
+ * A program that uses process.stdin to read user input.
+ */
+process.stdout.write('Welcome to ALX, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
