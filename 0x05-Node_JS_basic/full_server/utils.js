96c396ea91b1f22d6b5aca83b0100ffa185c386b
Achrafsadeq <achrafsadeqq@gmail.com>
2025-06-10 15:16:58 -0700

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) reject(Error('Cannot load the database'));
+
+      const lines = data.trim().split('\n').slice(1);
+      const students = {};
+
+      for (const line of lines) {
+        const [firstname, , , field] = line.split(',');
+        if (!students[field]) students[field] = [];
+        students[field].push(firstname);
+      }
+
+      resolve(students);
+    });
+  });
+}
+
+module.exports = readDatabase;
